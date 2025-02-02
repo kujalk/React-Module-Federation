88291f4d8cc0a85dc9051acfee409fa75eb01667
Janarthanan Kugathasan <jana19930918@gmail.com>
2025-02-02 22:10:11 +0800

    Added path to webconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 // In parent-app/webpack.config.js
+const path = require('path');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
